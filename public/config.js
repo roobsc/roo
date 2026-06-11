1bbd59d20a5b3730f16ed22784aa935637daadae
jbhbsc <bilinq1213@gmail.com>
2026-06-11 20:11:57 +0800

    Update launchpad address to 0x45431D

--- public/config.js
+++ public/config.js
@@ -1,7 +1,7 @@
 window.LAUNCHPAD_CONFIG = {
   chainId: 56,
   chainName: "BNB Smart Chain",
-  launchpadAddress: "0x3c5BF855d59af5FB6FfB67ae8e961Af04B4a3C88",
+  launchpadAddress: "0x45431DD7247F6F8D82Ad46690592B6a544CB8665",
   legacyLaunchpadAddresses: [],
   platformFeeWallet: "0x05aA5C72a716D9c7EE93802F779fFF998520d74e",
   pancakeRouter: "0x10ED43C718714eb63d5aA57B78B54704E256024E",
